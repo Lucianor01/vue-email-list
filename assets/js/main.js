410e7a7a78d998868730faa6e4750a48c2d649d1
Lucianor01 <lucianorosario02@gmail.com>
2023-04-21 16:11:05 +0200

    Fix apiEmail

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,8 @@ createApp({
             for (let i = 0; i < 10; i++) {
                 axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
                 .then((dati) => {
-                    this.emails.push(dati.data)
-                    console.log(dati.data)
+                    this.emails.push(dati.data.response)
+                    console.log(dati.data.response)
                 })
             }
         }
